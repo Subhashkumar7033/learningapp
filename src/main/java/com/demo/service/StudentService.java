167488edc5d2cd9d441c5e27ef4c75c694a84cc1
Subhash Kumar <subhash.kumar3@ibm.com>
2025-01-25 15:42:38 +0530

    This is my 2nd commit

--- src/main/java/com/demo/service/StudentService.java
+++ src/main/java/com/demo/service/StudentService.java
@@ -1,5 +1,9 @@
 package com.demo.service;
 
 public class StudentService {
+	public StudentService() {
+		// TODO Auto-generated constructor stub
+		System.out.println("This is my 1st service class");
+	}
 
 }
