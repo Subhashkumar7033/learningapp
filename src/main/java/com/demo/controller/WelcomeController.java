af057aa82be49c3b4bcbdf1eb87391810def7022
Subhash Kumar <subhash.kumar3@ibm.com>
2025-01-25 16:12:31 +0530

    This is my 2nd commit

--- src/main/java/com/demo/controller/WelcomeController.java
+++ src/main/java/com/demo/controller/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
     public WelcomeController() {
-      System.out.println("This is no Arg Constructor!")
+      System.out.println("This is no Arg Constructor!");
     }
 // This is Get api to display the welcome message
     @GetMapping("/")
